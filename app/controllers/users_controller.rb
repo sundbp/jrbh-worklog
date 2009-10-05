b30d45bb6bb96f8bd5cff6ee41b2f6e8bad31365
sundbp <patrik.sundberg@gmail.com>
2009-10-05 13:41:11 +0100

    authlogic related stuff.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,80 @@
+class UsersController < ApplicationController
+
+  before_filter :find_user
+
+  USERS_PER_PAGE = 20
+
+  def create
+    @user = User.new(params[:user])
+    respond_to do |format|
+      if @user.save
+        flash[:notice] = 'User was successfully created.'
+        format.html { redirect_to @user }
+        format.xml  { render :xml => @user, :status => :created, :location => @user }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @user.destroy
+        flash[:notice] = 'User was successfully destroyed.'        
+        format.html { redirect_to users_path }
+        format.xml  { head :ok }
+      else
+        flash[:error] = 'User could not be destroyed.'
+        format.html { redirect_to @user }
+        format.xml  { head :unprocessable_entity }
+      end
+    end
+  end
+
+  def index
+    @users = User.paginate(:page => params[:page], :per_page => USERS_PER_PAGE)
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @users }
+    end
+  end
+
+  def edit
+  end
+
+  def new
+    @user = User.new
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @user }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @user }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to @user }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id]) if params[:id]
+  end
+
+end
---
+++ db/migrate/20091005101347_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :login
+      t.string :alias
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistance_token
+      
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
