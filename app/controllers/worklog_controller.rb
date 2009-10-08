613e678d8dca6a39aa5c1ff4126068cc8a2a1c9b
sundbp <patrik.sundberg@gmail.com>
2009-10-08 23:03:37 +0100

    first cut of including the weekcalendar.

--- app/controllers/worklog_controller.rb
+++ app/controllers/worklog_controller.rb
@@ -1,5 +1,10 @@
 class WorklogController < ApplicationController
+  before_filter :require_user
+
+  layout "worklog"
+
   def edit
+    render :action => "edit", :layout => "worklog-edit"
   end
 
 end
