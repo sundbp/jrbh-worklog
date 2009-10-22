7d3464619b8e4647dab3850de9ded94cbd0b605b
sundbp <patrik.sundberg@gmail.com>
2009-10-22 13:06:57 +0100

    froze gems and added capistrano for deployment.

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+set :user,        "patrik"
+set :domain,      "bob"
+set :application, "jrbh-worklog"
+
+set :repository,  "git@github.com:sundbp/#{application}.git"
+set :deploy_to,   "/home/#{user}/www"
+set :use_sudo,    false
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, domain                         # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+default_run_options[:pty] = true
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+# change this path to where your dump is
+dump_file = "/Users/patrik/Documents/Worklog/worklog_total.dump"
+
+WorkPeriodStruct = Struct.new(:start,:end,:user,:task,:comment)
+
+# various mappings 
+alias_to_user = {
+        "JH" => "jenharris",
+        "JO" => "josbaldaston",
+        "MS" => "msim",
+        "PC" => "pcroney",
+        "HL" => "hleivers"
+        }
+
+File.open(dump_file) do |f|
+  data = Marshal.load(f)
+end
+
