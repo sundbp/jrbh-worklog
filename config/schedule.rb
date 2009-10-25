359cf5ed8fb1c04e030e5b2357e918e63f118237
sundbp <patrik.sundberg@gmail.com>
2009-10-25 12:34:38 +0000

    more days backup of DB.

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,11 @@
 
 set :output, "/home/patrik/www/jrbh-worklog/current/log/cron_log.log"
 
-every 1.day, :at => '12am' do
-  command "rm -f /home/patrik/Dropbox/jrbh_prod.dump"
+every 1.day, :at => '1pm' do
+  command "mv /home/patrik/Dropbox/jrbh_prod.dump /home/patrik/Dropbox/jrbh_prod.dump.1"
+  command "mv /home/patrik/Dropbox/jrbh_prod.dump.1 /home/patrik/Dropbox/jrbh_prod.dump.2"
+  command "mv /home/patrik/Dropbox/jrbh_prod.dump.2 /home/patrik/Dropbox/jrbh_prod.dump.3"
+  command "mv /home/patrik/Dropbox/jrbh_prod.dump.3 /home/patrik/Dropbox/jrbh_prod.dump.4"
+  command "mv /home/patrik/Dropbox/jrbh_prod.dump.4 /home/patrik/Dropbox/jrbh_prod.dump.5"
   command "/usr/bin/pg_dump jrbh_prod > /home/patrik/Dropbox/jrbh_prod.dump"
 end
