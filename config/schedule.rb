f4b60ff9b3371fbc56d73c98f113efdb7f9d5889
sundbp <patrik.sundberg@gmail.com>
2009-11-03 10:09:01 +0000

    change backup path.

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,10 +3,10 @@
 #set :output, "/home/patrik/www/jrbh-worklog/current/log/cron_log.log"
 
 every 1.day, :at => '1pm' do
-  command "mv /home/patrik/Dropbox/jrbh_prod.dump /home/patrik/Dropbox/jrbh_prod.dump.1"
-  command "mv /home/patrik/Dropbox/jrbh_prod.dump.1 /home/patrik/Dropbox/jrbh_prod.dump.2"
-  command "mv /home/patrik/Dropbox/jrbh_prod.dump.2 /home/patrik/Dropbox/jrbh_prod.dump.3"
-  command "mv /home/patrik/Dropbox/jrbh_prod.dump.3 /home/patrik/Dropbox/jrbh_prod.dump.4"
-  command "mv /home/patrik/Dropbox/jrbh_prod.dump.4 /home/patrik/Dropbox/jrbh_prod.dump.5"
-  command "/usr/bin/pg_dump jrbh_prod > /home/patrik/Dropbox/jrbh_prod.dump"
+  command "mv /home/patrik/Dropbox/backup/jrbh_prod.dump /home/patrik/Dropbox/backup/jrbh_prod.dump.1"
+  command "mv /home/patrik/Dropbox/backup/jrbh_prod.dump.1 /home/patrik/Dropbox/backup/jrbh_prod.dump.2"
+  command "mv /home/patrik/Dropbox/backup/jrbh_prod.dump.2 /home/patrik/Dropbox/backup/jrbh_prod.dump.3"
+  command "mv /home/patrik/Dropbox/backup/jrbh_prod.dump.3 /home/patrik/Dropbox/backup/jrbh_prod.dump.4"
+  command "mv /home/patrik/Dropbox/backup/jrbh_prod.dump.4 /home/patrik/Dropbox/backup/jrbh_prod.dump.5"
+  command "/usr/bin/pg_dump jrbh_prod > /home/patrik/Dropbox/backup/jrbh_prod.dump"
 end
