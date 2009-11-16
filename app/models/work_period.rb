3b70c2a446b8d10984d1dd81e04065a0ab7a9267
sundbp <patrik.sundberg@gmail.com>
2009-11-16 18:50:13 +0000

    add validations so we don't end up with broken periods in the DB.

--- app/models/work_period.rb
+++ app/models/work_period.rb
@@ -2,7 +2,18 @@ class WorkPeriod < ActiveRecord::Base
   belongs_to :user
   belongs_to :worklog_task
 
+  validate :correct_period?
+
   def company
     worklog_task.company
   end
+
+private
+    def correct_period?
+      if start >= attributes['end']
+        errors.add(:end, " is before (or equal) to start. Negative time does not exist!")
+      elsif start.to_date != attributes['end'].to_date
+        errors.add_to_base("start and end need to be on the same day, no spanning of midnight allowed!")
+      end
+    end
 end
