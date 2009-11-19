9f90c87b488d3153712637dddcc61e7cb14016ed
sundbp <patrik.sundberg@gmail.com>
2009-11-19 18:18:35 +0000

    small change to give more info on number of users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,8 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(:page => params[:page], :per_page => USERS_PER_PAGE, :order => 'alias ASC')
+    @all_users = User.find(:all)
+    @active_users = User.active_employees
     respond_to do |format|
       format.html
       format.xml  { render :xml => @users }
