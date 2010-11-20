bd8949e946c061a4e5cb7f5e778818c329aa0c6a
sundbp <patrik.sundberg@gmail.com>
2010-11-20 17:21:38 +0000

    dont mess up existing crontab until ready to go into prod.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ set :deploy_to,   "/home/#{user}/www/production/#{application}"
 set :rails_env,   :production
 
 # only run whenever in prod
-set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
+#set :whenever_command, "bundle exec whenever"
+#require "whenever/capistrano"
