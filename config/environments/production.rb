37825ac39a08fbbacf73cdbcd6fe4d0c3b32fdd9
sundbp <patrik.sundberg@gmail.com>
2010-11-28 14:04:14 +0000

    run prod and staging on different ports, adapt asset related things.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,7 @@ JrbhWorklog::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  # TODO: when serving both staging and prod i dont want to have apache serve the assets
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
